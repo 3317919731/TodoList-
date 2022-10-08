5287f84cccdfb73643ec01020da613f8bec67616
jwentong <3317919731@qq.com>
2022-10-08 14:44:18 +0800

    变更

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,125 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <Header :addTodo="addTodo"></Header>
+        <List :todos="todos" :checkTodo="checkTodo" :deleteTodo="deleteTodo"></List>
+        <Footer :todos="todos" :checkAllTodo="checkAllTodo" :clearAllTodo="clearAllTodo"></Footer>
+      </div>
+    </div>
   </div>
-</template>
 
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import List from "@/components/List";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+  name: "App",
+  data() {
+    return {
+      todos: [
+        {id: '001', title: '吃饭', done: true},
+        {id: '002', title: '睡觉', done: true},
+        {id: '003', title: '开车', done: true},
+      ]
+    }
+  },
+  components: {Header, Footer, List},
+  methods: {
+    //添加一个todo
+    addTodo(todoObj) {
+      this.todos.unshift(todoObj)
+    },
+    //勾选or取消一个todo
+    checkTodo(id) {
+      this.todos.forEach((todo) => {
+        if (todo.id === id) {
+          todo.done = !todo.done
+        }
+      })
+    },
+    //删除一个todo
+    deleteTodo(id) {
+      this.todos = this.todos.filter((todo) => {
+        return todo.id !== id
+      })
+    },
+    //全选or全不选
+    checkAllTodo(done) {
+      this.todos.forEach((todo) => {
+        todo.done = done
+      })
+    },
+    //清楚已经完成的
+    clearAllTodo() {
+      this.todos = this.todos.filter((todo) => {
+        return !todo.done
+      })
+    }
   }
+
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background: #fff;
 }
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-edit {
+  color: #fff;
+  background-color: lightgreen;
+  border: 1px solid green;
+  margin-right: 5px;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn-edit:hover {
+  color: #fff;
+  background-color: green;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
 
+//引入Vue
+import Vue from 'vue'
+
+//引入App
+import App from './App'
 Vue.config.productionTip = false
 
+//创建Vue
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el:'#root',
+    render: h=>h(App)
+})
